03fdbb4873de9dcbd21803e06acb906fcd06dcd3
Raghav <vahgarkhurana@gmail.com>
2018-01-31 16:09:56 +0530

    Image Carousel Left

--- ishan_creations/ishan_creations/urls.py
+++ ishan_creations/ishan_creations/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 urlpatterns = [
     # Examples:
     url(r'^$', 'index.views.index', name='homepage'),
-    url(r'^icbc$', 'index.views.icbc', name='icbc'),
+    url(r'^members$', 'index.views.icbc', name='icbc'),
     url(r'^categories$', 'entity.views.list_city', name='categories'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^entity/', include('entity.urls', namespace='entity')),
